1de7af0dd5254dd5a4f324d5130385855e0ab619
Mupparapu Venu Prasad <155827600+Venu-Prasad2@users.noreply.github.com>
2024-12-07 13:39:24 +0530

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -28,7 +28,7 @@ const Login = () => {
             return;
         }
 
-        const res = await fetch('http://localhost:5000/api/login', {
+        const res = await fetch('https://my-personal-blog-takg.onrender.com/api/login', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ username, password }),
